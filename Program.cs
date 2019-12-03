a0c860bcd9a0a676fc82c1854a884fffa46f7d27
tomgozdek@gmail.com <tomgozdek@gmail.com>
2019-12-03 16:09:29 +0100

    First commit of simple dotnet project for teamcity

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,\nPlease type you name");
+            string input = Console.ReadLine();
+            Console.WriteLine("Nice to meet you {0}!", input);
+            Console.WriteLine("The current date is " + DateTime.Now);
+        }
+    }
+}
